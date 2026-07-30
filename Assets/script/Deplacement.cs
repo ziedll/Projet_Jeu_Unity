cfe7678633ab847e0e7b4c7fabc5a5b5cc1edf22
ziedll <112785620+ziedll@users.noreply.github.com>
2026-07-30 12:00:18 +0200

    reglage des animations de personnage, caméra, gravité, collisions avec la scène et de la porte pour qu'elle redirige vers la scène du level

--- Assets/script/Deplacement.cs
+++ Assets/script/Deplacement.cs
@@ -1,10 +1,32 @@
+using TMPro;
 using UnityEngine;
 
 public class deplacement : MonoBehaviour
 {
+    [Header ("Déplacements")]
     public float vitesse = 6f;
     public float vitesseRotation = 10f;
     public Animator animator;
+
+    // Variables pour le suivi de la caméra
+    [Header("Paramètres de la Caméra")]
+    [SerializeField] private Transform _cameraTransform;
+    public Vector3 cameraOffset = new Vector3(0.18f, 3.5f, -8f);
+    public float cameraSmoothSpeed = 5f;
+
+    // Ajout d'une référence au composant Character Controller
+    private CharacterController controller;
+
+    [Header("Saut et Gravité")]
+    public float hauteurSaut = 3.5f;
+    public float gravite = -9.81f;
+    private Vector3 velocite;
+
+    void Start()
+    {
+        // On récupère le composant au lancement du jeu
+        controller = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
@@ -16,6 +38,9 @@ public class deplacement : MonoBehaviour
         if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) x = -1f;
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) x = 1f;
 
+        Vector3 mouseP = new Vector3(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height, 0f);
+        Debug.Log(mouseP);
+        _cameraTransform.rotation = Quaternion.Euler(mouseP);
         Vector3 dir = new Vector3(x, 0f, z).normalized;
 
         if (dir.magnitude >= 0.1f)
@@ -23,13 +48,48 @@ public class deplacement : MonoBehaviour
             Quaternion targetRotation = Quaternion.LookRotation(dir);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * vitesseRotation);
 
-            transform.Translate(dir * vitesse * Time.deltaTime, Space.World);
-            
+        
+            controller.Move(dir * vitesse * Time.deltaTime);
+
             if (animator != null) animator.SetBool("IsRunning", true);
         }
         else
         {
             if (animator != null) animator.SetBool("IsRunning", false);
+        }
+
+        // Vérifie si on touche le sol et qu'on ne monte plus
+        if (controller.isGrounded && velocite.y < 0)
+        {
+            velocite.y = -2f; // Petite force pour maintenir le personnage collé au sol
+        }
+
+        // Action de sauter
+        if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+        {
+            velocite.y = Mathf.Sqrt(hauteurSaut * -2f * gravite);
+            if(animator != null)
+            {
+                animator.SetTrigger("Jump");
+            }
+        }
+
+        // Ajout constant de la gravité 
+        velocite.y += gravite * Time.deltaTime;
+
+        // Application de ce mouvement vertical
+        controller.Move(velocite * Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+        // Fait suivre la caméra de manière fluide après que le personnage ait bougé
+        if (_cameraTransform != null)
+        {
+            Vector3 desiredPosition = transform.position + cameraOffset;
+            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, desiredPosition, cameraSmoothSpeed * Time.deltaTime);
+            _cameraTransform.LookAt(transform.position + Vector3.up * 1.5f);
+        
         }
     }
 }
